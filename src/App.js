cd5189042792f8016a50ab819888f2bd141056a3
caiqupedasilva <caiquemaverik2@gmail.com>
2022-12-01 17:30:27 -0300

    Add lista de times.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario'
+import Time from './componentes/Time';
 
 function App() {
 
-  const  [colaboradores, setColaboradores] = useState([])
+  const times = [
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE95F5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
 
-  const aoNovoColaboradorAdicionado = (colaborador) =>{
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
@@ -14,7 +48,8 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
